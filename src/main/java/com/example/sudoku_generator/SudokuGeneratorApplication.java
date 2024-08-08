f14ef1f7583a7cdc80651079f8cff327ecbdc415
ueno61 <107382274+ueno61@users.noreply.github.com>
2024-08-09 08:23:42 +0900

    setup

---
+++ src/main/java/com/example/sudoku_generator/SudokuGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.sudoku_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudokuGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudokuGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sudoku_generator/SudokuGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sudoku_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
